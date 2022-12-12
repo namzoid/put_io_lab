53941d90dd0dd9bf0dc21727b4e7d63d97e70e1b
Darya Makarevich <48219218+namzoid@users.noreply.github.com>
2022-12-12 01:12:28 +0100

    Add files via upload

---
+++ 08-junit/AudiobookPriceCalculatorTest.java
@@ -0,0 +1,50 @@
+package put.io.testing.audiobooks;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// Pytanie 5.1: typ testowania
+// Pytanie 5.2: 4 możliwe ścieżki działania
+
+class AudiobookPriceCalculatorTest extends AudiobookPriceCalculator{
+
+    private AudiobookPriceCalculator calculator;
+    private Audiobook audiobook;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new AudiobookPriceCalculator();
+        audiobook =  new Audiobook("Book", 10);
+
+    }
+
+    // przypadki testowe:
+
+    @Test
+    void testIsSubscribed() {
+        Customer customer = new Customer("Customer1", Customer.LoyaltyLevel.SILVER, true);
+        assertEquals(0.0, calculator.calculate(customer, audiobook));
+    }
+
+
+    @Test
+    void testLoyaltySTANDART() {
+        Customer customer = new Customer("Customer2", Customer.LoyaltyLevel.STANDARD, false);
+        assertEquals(audiobook.getStartingPrice(), calculator.calculate(customer, audiobook));
+    }
+
+
+    @Test
+    void testLoyaltySILVER() {
+        Customer customer = new Customer("Customer3", Customer.LoyaltyLevel.SILVER, false);
+        assertEquals(audiobook.getStartingPrice()*0.9, calculator.calculate(customer, audiobook));
+    }
+
+
+    @Test
+    void testLoyaltyGOLD() {
+        Customer customer = new Customer("Customer4", Customer.LoyaltyLevel.GOLD, false);
+        assertEquals(audiobook.getStartingPrice()*0.8, calculator.calculate(customer, audiobook));
+    }
+
+}
